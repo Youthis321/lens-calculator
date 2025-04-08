c27ca5f5a393575c2e8117420ba8f792a6dda5a5
bumiagung.pusat <bumiagung.pusat@gmail.com>
2025-04-09 00:54:45 +0700

    Add doc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,20 @@
-<head>
-  <link rel="manifest" href="/manifest.json" />
-  <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-  <meta name="theme-color" content="#0d6efd" />
-</head>
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Meta & PWA Links */}
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+          <meta name="theme-color" content="#0d6efd" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
