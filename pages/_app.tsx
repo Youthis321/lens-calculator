6b3d75ee889c14bbc8c7709376ef8dda504cdfad
bumiagung.pusat <bumiagung.pusat@gmail.com>
2025-05-21 08:42:41 +0700

    add feature SpeedInsights by vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import OneSignalProvider from './OneSignalProvider';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -59,6 +60,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <OneSignalProvider />
 
       {/* Tombol Toggle Tema */}
